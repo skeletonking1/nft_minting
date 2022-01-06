c7847c69022a190994b6ffa53d6ca02464e445e6
skeletonking1 <blue.dragon980511@gmail.com>
2022-01-07 07:37:11 +0900

    init

---
+++ src/redux/blockchain/blockchainActions.js
@@ -0,0 +1,126 @@
+// constants
+import Web3EthContract from "web3-eth-contract";
+import Web3 from "web3";
+// log
+import { fetchData } from "../data/dataActions";
+
+const connectRequest = () => {
+  return {
+    type: "CONNECTION_REQUEST",
+  };
+};
+
+const connectSuccess = (payload) => {
+  return {
+    type: "CONNECTION_SUCCESS",
+    payload: payload,
+  };
+};
+
+const connectFailed = (payload) => {
+  return {
+    type: "CONNECTION_FAILED",
+    payload: payload,
+  };
+};
+
+const updateAccountRequest = (payload) => {
+  return {
+    type: "UPDATE_ACCOUNT",
+    payload: payload,
+  };
+};
+
+export const connect = () => {
+  return async (dispatch) => {
+    dispatch(connectRequest());
+    const abiResponse = await fetch("/config/abi.json", {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    });
+    const abi = await abiResponse.json();
+    const configResponse = await fetch("/config/config.json", {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    });
+    const CONFIG = await configResponse.json();
+
+    let ethereum;
+    try {
+      ethereum = await window.web3Modal.connect();
+      window.provider = ethereum;
+    } catch (e) {
+      console.log("Could not get a wallet connection", e);
+      return;
+    }
+    // providerObj.on("accountsChanged", (accounts) => {
+    //   console.log(`accountsChanged = ${accounts}`);
+    //   //fetchAccountData();
+    // });
+    // providerObj.on("chainChanged", (chain_id) => {
+    //   console.log(`chainChanged ${chain_id}`);
+    //   fetchAccountData();
+    // });
+
+    // providerObj.on("disconnect", (error) => {
+    //   console.log(`disconnect ${error}`);
+    //   onDisconnect();
+    // });
+    // await fetchAccountData();
+
+
+    // const { ethereum } = window;
+    // const metamaskIsInstalled = ethereum && ethereum.isMetaMask;
+    // if (metamaskIsInstalled) {
+    console.log(ethereum, "======")
+      Web3EthContract.setProvider(ethereum);
+      let web3 = new Web3(ethereum);
+      try {
+        const accounts = await ethereum.request({
+          method: "eth_requestAccounts",
+        });
+        const networkId = await ethereum.request({
+          method: "net_version",
+        });
+        if (networkId == CONFIG.NETWORK.ID) {
+          const SmartContractObj = new Web3EthContract(
+            abi,
+            CONFIG.CONTRACT_ADDRESS
+          );
+          dispatch(
+            connectSuccess({
+              account: accounts[0],
+              smartContract: SmartContractObj,
+              web3: web3,
+            })
+          );
+          // Add listeners start
+          ethereum.on("accountsChanged", (accounts) => {
+            dispatch(updateAccount(accounts[0]));
+          });
+          ethereum.on("chainChanged", () => {
+            window.location.reload();
+          });
+          // Add listeners end
+        } else {
+          dispatch(connectFailed(`Change network to ${CONFIG.NETWORK.NAME}.`));
+        }
+      } catch (err) {
+        dispatch(connectFailed("Something went wrong."));
+      }
+    // } else {
+    //   dispatch(connectFailed("Install Metamask."));
+    // }
+  };
+};
+
+export const updateAccount = (account) => {
+  return async (dispatch) => {
+    dispatch(updateAccountRequest({ account: account }));
+    dispatch(fetchData(account));
+  };
+};
